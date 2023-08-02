806719a1f2e2b2ff1e93b5448f93e320171a9b23
Alyssa Geria <alyssageria@gmail.com>
2023-08-02 11:08:07 -0700

    started navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import geriaLogo from '../images/geria-logo-2.png'
 
 function Navbar({ currentPage, handlePageChange }) {
     return (
         <div>
-
+            <div className='navbar p-5 bg-black flex justify-between'>
+                <img src={geriaLogo} className='geria-logo pt-1'></img>
+                <ul className='text-white flex items-center text-xl'>
+                    <li className='p-3'>
+                        <button
+                            id='nav-button'
+                            onClick={() => handlePageChange('Welcome')}
+                            className={currentPage === 'Welcome' ? 'active' : ''}
+                        >Welcome</button></li>
+                    <li className='p-3'>
+                        <button
+                            id='nav-button'
+                            onClick={() => handlePageChange('Projects')}
+                            className={currentPage === 'Projects' ? 'active' : ''}>
+                            Projects</button></li>
+                    <li className='p-3'>
+                        <button
+                            id='nav-button'
+                            onClick={() => handlePageChange('Contact')}
+                            className={currentPage === 'Contact' ? 'active' : ''}>
+                            Contact</button></li>
+                    <li className='p-3'>
+                        <button
+                            id='nav-button'
+                            onClick={() => handlePageChange('Resume')}
+                            className={currentPage === 'Resume' ? 'active' : ''}>
+                            Resume</button></li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/components/pages/Welcome.js
+++ src/components/pages/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Welcome() {
     return (
         <div>
-            <h1>test</h1>
+
         </div>
     )
 }
